a57458c6361d9f7448977ba439386c380c1e8cd0
Tomasz Kowalski <tomkowa64@gmail.com>
2021-07-22 12:06:23 +0200

    Footer Design

--- trade-away/src/components/Footer.vue
+++ trade-away/src/components/Footer.vue
@@ -1,3 +1,18 @@
 <template>
-    <h1>This is footer</h1>
+    <section class="footer">
+        <div class="footer-title">TradeAway</div>
+        <div class="footer-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas 
+            purus viverra accumsan in nisl nisi. Nisl suscipit adipiscing bibendum est
+             ultricies integer quis auctor. Justo donec enim diam vulputate ut. Imperdiet 
+             nulla malesuada pellentesque elit eget gravida. Nunc aliquet bibendum enim facilisis 
+             gravida. In dictum non consectetur a erat nam. 
+        </div>
+        <div class="footer-icons-group">
+            <i class="fab fa-facebook-square"></i>
+            <i class="fab fa-instagram-square"></i>
+            <i class="fab fa-twitter-square"></i>
+            <i class="fab fa-linkedin"></i>
+        </div>
+    </section>
 </template>
