e94252d1a41e013d0746e89c3540db3415900040
Tomasz Kowalski <tomkowa64@gmail.com>
2021-07-22 11:32:40 +0200

    Navbar design

--- trade-away/src/components/Nav.vue
+++ trade-away/src/components/Nav.vue
@@ -7,12 +7,10 @@
         </div>
         <div class="company-name"><span style="color: #84d6e3">Trade</span><span style="color: #d9d8e3">Away</span></div>
         <div class="menu">
-            <div class="menu-item">
-                <font-awesome-icon icon="user-secret" />
-            </div>
-            <div class="menu-item">About</div>
-            <div class="menu-item">Contact</div>
-            <div class="menu-item">Account</div>
+            <div class="menu-item"><i class="fas fa-shopping-cart"></i></div>
+            <div class="menu-item"><a href="/about">About</a></div>
+            <div class="menu-item"><a href="/contact">Contact</a></div>
+            <div class="menu-item"><a href="/Account">Account</a></div>
         </div>
     </section>
 </template>
--- trade-away/src/main.js
+++ trade-away/src/main.js
@@ -3,15 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
-
-library.add(faUserSecret)
-
 createApp(App)
-.use(FontAwesomeIcon)
 .use(store)
 .use(router)
 .mount('#app')
