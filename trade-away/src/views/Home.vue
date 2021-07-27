76ff853aabd116534f923c17dcdeb53225d49540
Tomasz Kowalski <tomkowa64@gmail.com>
2021-07-27 11:12:05 +0200

    Trending section created & designed

--- trade-away/src/views/Home.vue
+++ trade-away/src/views/Home.vue
@@ -5,8 +5,54 @@
       <div>
         Trending | <span class="tag-show-all"><a href="">Show All <i class="fas fa-arrow-down"></i></a></span>
       </div>
-      <div class="product-list">
-        <!-- Product list items here TODO -->
+    </div>
+    <div class="product-list">
+      <div class="product-list-item" style="background: rgb(67, 214, 202);">
+        <img src="../assets/backpack.png" alt="">
+        <div class="product-list-item-name">Product Name</div>
+        <div class="product-list-item-desc">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+          sed do eiusmod tempor incididunt ut labore et dolore.
+        </div>
+        <div class="product-list-item-price">
+          69.99$
+        </div>
+      </div>
+
+      <div class="product-list-item" style="background: #f06f1f">
+        <img src="../assets/blender.png" alt="">
+        <div class="product-list-item-name">Product Name</div>
+        <div class="product-list-item-desc">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+          sed do eiusmod tempor incididunt ut labore et dolore.
+        </div>
+        <div class="product-list-item-price">
+          29.99$
+        </div>
+      </div>
+
+      <div class="product-list-item" style="background: #f5b48c">
+        <img src="../assets/shoe.png" alt="">
+        <div class="product-list-item-name">Product Name</div>
+        <div class="product-list-item-desc">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+          sed do eiusmod tempor incididunt ut labore et dolore.
+        </div>
+        <div class="product-list-item-price">
+          30.99$
+        </div>
+      </div>
+
+      <div class="product-list-item" style="background: #15a123">
+        <img src="../assets/dress.png" alt="">
+        <div class="product-list-item-name">Product Name</div>
+        <div class="product-list-item-desc">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+          sed do eiusmod tempor incididunt ut labore et dolore.
+        </div>
+        <div class="product-list-item-price">
+          119.99$
+        </div>
       </div>
     </div>
   </section>
@@ -23,6 +69,6 @@ export default {
   }
 }
 </script>
-<style scoped>
+<style  lang="scss">
 @import url('../styles/Home.scss');
 </style>
