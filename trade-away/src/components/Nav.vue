f9113d7465c085f6baa420847350d3066aa8e0f6
Tomasz Kowalski <tomkowa64@gmail.com>
2021-08-02 10:29:47 +0200

    Navbar & Color theme changes

--- trade-away/src/components/Nav.vue
+++ trade-away/src/components/Nav.vue
@@ -1,15 +1,12 @@
 <template>
     <section class="navbar">
-        <div class="logo">
-            <a href="/">
-                <img alt="logo" src="../assets/logo.png">
-            </a>
-        </div>
         <div class="company-name">
             <div class="menu-toggle">
                 <i class="fas fa-bars toggler"></i>
             </div>
-            <span style="color: #84d6e3">Trade</span><span style="color: #d9d8e3">Away</span>
+            <a href="/">
+                <span class="logo-font" style="color: #3267ba; opacity: 0.8">Trade</span><span class="logo-font" style="color: #cf4e6c; opacity: 0.8">Away</span>
+            </a>  
         </div>
         <div class="menu menu-display">
             <div class="menu-item cart-icon">
@@ -19,9 +16,9 @@
                     <!-- TODO Product count function -->
                 </div>
             </div>
-            <div class="menu-item"><a href="/about">About</a></div>
-            <div class="menu-item"><a href="/contact">Contact</a></div>
-            <div class="menu-item"><a href="/Account">Account</a></div>
+            <div class="menu-item"><a href="#/about">About</a></div>
+            <div class="menu-item"><a href="#/contact">Contact</a></div>
+            <div class="menu-item"><a href="#/Account">Account</a></div>
         </div>
     </section>
 </template>
