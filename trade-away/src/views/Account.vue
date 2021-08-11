7a32e4341256a813eeb96bd58a44fdf74d8613fd
Tomasz Kowalski <tomkowa64@gmail.com>
2021-08-11 11:15:42 +0200

    Account Page - Person Details

--- trade-away/src/views/Account.vue
+++ trade-away/src/views/Account.vue
@@ -12,7 +12,55 @@
                 </div>
             </div>
             <div class="person-details">
-                b
+            <!-- Person info -->
+                <div class="person-info-box">
+                    <div class="content">
+                        <div class="icon-label">
+                            <i class="fas fa-user-alt"></i>
+                        </div>
+                        <div class="info-text">
+                            <b>Name:</b> User
+                        </div>
+                        <div class="info-text">
+                            <b>Surname:</b> Name
+                        </div>
+                        <div class="info-text">
+                            <b>Age:</b> 12
+                        </div>
+                        <div class="info-text">
+                            <b>E-mail:</b> sample@example.com
+                        </div>
+                        <div class="info-text">
+                            <b>PESEL:</b> 12345678901
+                        </div>
+                    </div>
+                    <div class="edit">
+                        <i class="fas fa-edit"></i>
+                    </div>
+                </div>
+                <!-- Address info -->
+                <div class="person-info-box">
+                    <div class="content">
+                        <div class="icon-label">
+                            <i class="fas fa-envelope"></i>
+                        </div>
+                        <div class="info-text">
+                            <b>Country:</b> Poland
+                        </div>
+                        <div class="info-text">
+                            <b>City:</b> Rzeszow
+                        </div>
+                        <div class="info-text">
+                            <b>Postal Code:</b> 35-123
+                        </div>
+                        <div class="info-text">
+                            <b>Address:</b> Hetmanska 123
+                        </div>
+                    </div>
+                    <div class="edit">
+                        <i class="fas fa-edit"></i>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
