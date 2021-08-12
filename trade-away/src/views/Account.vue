54909be89f5fa62c4308e709741cb1796ef2acc9
Tomasz Kowalski <tomkowa64@gmail.com>
2021-08-12 09:58:02 +0200

    Account history & Tags section

--- trade-away/src/views/Account.vue
+++ trade-away/src/views/Account.vue
@@ -63,6 +63,110 @@
                 </div>
             </div>
         </div>
+        <!-- Account history -->
+        <div class="account-history">
+            <div class="section-label">
+                <div>
+                    Account History | <span class="tag-show-all"><a href="">Show All <i class="fas fa-arrow-down"></i></a></span>
+                </div>
+            </div>
+            <div class="product-list">
+            <div class="product-list-item" style="background: rgb(67, 214, 202);">
+                <img src="../assets/backpack.png" alt="">
+                <div class="product-list-item-name">Product Name</div>
+                <div class="product-list-item-desc">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore.
+                </div>
+                <div class="product-list-item-price">
+                69.99$
+                </div>
+            </div>
+
+            <div class="product-list-item" style="background: #f06f1f">
+                <img src="../assets/blender.png" alt="">
+                <div class="product-list-item-name">Product Name</div>
+                <div class="product-list-item-desc">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore.
+                </div>
+                <div class="product-list-item-price">
+                29.99$
+                </div>
+            </div>
+
+            <div class="product-list-item" style="background: #f5b48c">
+                <img src="../assets/shoe.png" alt="">
+                <div class="product-list-item-name">Product Name</div>
+                <div class="product-list-item-desc">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore.
+                </div>
+                <div class="product-list-item-price">
+                30.99$
+                </div>
+            </div>
+
+            <div class="product-list-item" style="background: #71bf93">
+                <img src="../assets/dress.png" alt="">
+                <div class="product-list-item-name">Product Name</div>
+                <div class="product-list-item-desc">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore.
+                </div>
+                <div class="product-list-item-price">
+                119.99$
+                </div>
+            </div>
+            </div>
+        </div>
+
+        <!-- Tags -->
+        <div class="tags">
+            <div class="section-label">
+                <div>
+                    Tags <!-- input tag TODO -->
+                </div>
+            </div>
+            <div class="tags-list">
+                <div class="item">
+                    Sport
+                    <i class="far fa-times-circle"></i>
+                </div>
+                <div class="item">
+                    RTV/AGD
+                    <i class="far fa-times-circle"></i>
+                </div>
+                <div class="item">
+                    Furniture
+                    <i class="far fa-times-circle"></i>
+                </div>
+                <div class="item">
+                    Cloths
+                    <i class="far fa-times-circle"></i>
+                </div>
+                <div class="item">
+                    Office
+                    <i class="far fa-times-circle"></i>
+                </div>
+                <div class="item">
+                    Games
+                    <i class="far fa-times-circle"></i>
+                </div>
+                <div class="item">
+                    Accessories
+                    <i class="far fa-times-circle"></i>
+                </div>
+                <div class="item">
+                    Cosmetics
+                    <i class="far fa-times-circle"></i>
+                </div>
+                <div class="item">
+                    Vehicles
+                    <i class="far fa-times-circle"></i>
+                </div>
+            </div>
+        </div>
     </section>
 </template>
 <script>
