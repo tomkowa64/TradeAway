829cb2df2849540c774c82a8dd3a89f50874ee5c
Tomasz Kowalski <tomkowa64@gmail.com>
2021-08-13 11:15:53 +0200

    Product concept design

--- trade-away/src/views/Product.vue
+++ trade-away/src/views/Product.vue
@@ -1,3 +1,43 @@
 <template>
-    <h1>Product info</h1>
+    <section class="product">
+        <div class="general-info">
+            <div class="product-image">
+
+            </div>
+            <div class="product-general-information">
+
+            </div>
+        </div>
+        <div class="details">
+            <div class="detail">
+
+            </div>
+            <div class="detail">
+
+            </div>
+            <div class="detail">
+
+            </div>
+            <div class="detail">
+
+            </div>
+            <div class="detail">
+
+            </div>
+            <div class="detail">
+
+            </div>
+        </div>
+        <div class="seller">
+            <div class="seller-avatar">
+
+            </div>
+            <div class="seller-info">
+
+            </div>
+        </div>
+    </section>
 </template>
+<style lang="scss">
+    @import url('../styles/Product.scss');
+</style>
