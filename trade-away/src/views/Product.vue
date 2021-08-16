7edf02ca080148043c08ddcfc5e0dff0011651c8
Tomasz Kowalski <tomkowa64@gmail.com>
2021-08-16 11:24:49 +0200

    Product general info section

--- trade-away/src/views/Product.vue
+++ trade-away/src/views/Product.vue
@@ -2,10 +2,58 @@
     <section class="product">
         <div class="general-info">
             <div class="product-image">
-
+                <img src="../assets/dress.png" alt="">
             </div>
             <div class="product-general-information">
-
+                <div class="product-name">Dress Flowers Edition 2137</div>
+                <div class="product-price">49.99$</div>
+                <div class="sold-stock">200 people bought 400 items</div>
+                <div class="icons-group">
+                    <div class="item">
+                        <div class="icon"><i class="far fa-smile"></i></div>
+                        <div class="description">
+                            A lot of happy customers who bought this product
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="icon"><i class="fas fa-shipping-fast"></i></div>
+                         <div class="description">
+                            Fast shipment, order now and get your product in 1 day
+                        </div>
+                        <div class="more-about">
+                            Shippings
+                            <i class="fas fa-arrow-circle-right"></i>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="icon"><i class="fas fa-undo-alt"></i></div>
+                         <div class="description">
+                            You can get your money back if you are unsatisfied within 1 week
+                        </div>
+                        <div class="more-about">
+                            Returns
+                            <i class="fas fa-arrow-circle-right"></i>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="icon"><i class="fas fa-wallet"></i></div>
+                         <div class="description">
+                            Convenient payments
+                        </div>
+                        <div class="more-about">
+                            Payments
+                            <i class="fas fa-arrow-circle-right"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="quantity-input">
+                    <div class="plus"><i class="fas fa-plus"></i></div>
+                    <div class="quantity">0</div>
+                    <div class="minus off"><i class="fas fa-minus"></i></div>
+                </div>
+                <div class="buy-product-button">
+                    Buy
+                </div>
             </div>
         </div>
         <div class="details">
@@ -38,6 +86,16 @@
         </div>
     </section>
 </template>
+<script>
+import $ from 'jquery';
+
+export default {}
+
+$('.plus').on('click', () => {
+
+})
+</script>
+
 <style lang="scss">
     @import url('../styles/Product.scss');
 </style>
